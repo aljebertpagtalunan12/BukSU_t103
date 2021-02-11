aeef5d891d753ee57dee53bf035e7e5b455d7afa
aljebertpagtalunan12 <73680533+aljebertpagtalunan12@users.noreply.github.com>
2021-02-11 15:13:13 +0800

    Add files via upload

---
+++ Assignment.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+//write a C program that will display the sum,product,difference,quotient and remainder.
+int main() 
+{
+	int a,b,sum,product,difference,quotient,remainder;
+	printf("Enter 1st number : ");
+	scanf("%d",&a);
+	printf("Enter 2nd number ; ");
+	scanf("%d",&b);
+	sum= a+b;
+	product= a*b;
+	difference= a-b;
+	quotient= a/b;
+	remainder= a%b;
+	printf("Sum is: %d", sum);
+	printf("\nProduct is: %d", product);
+	printf("\nDifference is: %d", difference);
+	printf("\nQoutient is: %d", quotient);
+	printf("\Remainder is: %d", remainder);
+	return 0;
+			
+	
+}
